b53093e55fc04a285574fb3cbbca518cde9e065f
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-06-04 16:47:52 +0100

    Code Tidy

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,3 +1,6 @@
+// 1. Packages
+// -----------
+
 // @codekit-prepend "/node_modules/jquery/dist/jquery.js";
 // @codekit-prepend "/node_modules/foundation-sites/dist/js/foundation.js";
 // @codekit-prepend "/node_modules/motion-ui/dist/motion-ui.js";
@@ -9,41 +12,41 @@
 
 $(document).ready(function() {
 
-// 1. Foundation
-// -------------
+// 2. Foundation Queries
+// ---------------------
 
 Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
 Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
 
+// 3. Foundation
+// -------------
+
 $(document).foundation();
 
+// 4. Lightgallery
+// ---------------
 
 $('#video-gallery').lightGallery({
-   counter : false,
-    vimeoPlayerParams: {
-        byline : 0,
-        portrait : 0,
-        color : '272A67'
-    },
-    youtubePlayerParams: {
-        modestbranding: 1,
-        showinfo: 0,
-        rel: 0,
-        controls: 0
-    }
+  counter : false,
+  vimeoPlayerParams: {
+      byline : 0,
+      portrait : 0,
+      color : '272A67'
+  },
+  youtubePlayerParams: {
+      modestbranding: 1,
+      showinfo: 0,
+      rel: 0,
+      controls: 0
+  }
 }); 
 
-// 2. Cookies Policy
-// -----------------
 
 
-
-// 3. Swiper
+// 5. Swiper
 // ---------
-
-$(document).ready(function () {
 
 var swiper = new Swiper('.carousel--explore', {
   slidesPerView: 2,
@@ -52,28 +55,24 @@ var swiper = new Swiper('.carousel--explore', {
   loopFillGroupWithBlank: true,
   navigation: {
     nextEl: '.test-next',
-    prevEl: '.test-prev',
+    prevEl: '.test-prev'
   },
   breakpoints: {
     1440: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     1200: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     1024: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     640: {
-      slidesPerView: 1,
+      slidesPerView: 1
     }
   }
 })
 
-
-});
-
-$(document).ready(function () {
 
 var swiper = new Swiper('.carousel--matrix', {
   slidesPerView: 'auto',
@@ -82,13 +81,10 @@ var swiper = new Swiper('.carousel--matrix', {
   loop: true,
   navigation: {
     nextEl: '.test-next',
-    prevEl: '.test-prev',
+    prevEl: '.test-prev'
   },
 })
 
-});
-
-$(document).ready(function () {
 
 var swiper = new Swiper('.carousel--instagram', {
   slidesPerView: 5,
@@ -97,33 +93,30 @@ var swiper = new Swiper('.carousel--instagram', {
   loopFillGroupWithBlank: true,
   navigation: {
     nextEl: '.test-next',
-    prevEl: '.test-prev',
+    prevEl: '.test-prev'
   },
   breakpoints: {
     1440: {
-      slidesPerView: 4,
+      slidesPerView: 4
     },
     1200: {
-      slidesPerView: 4,
+      slidesPerView: 4
     },
     1024: {
-      slidesPerView: 2,
+      slidesPerView: 2
     },
     640: {
-      slidesPerView: 1,
+      slidesPerView: 1
     }
   }
 })
 
-});
-
-// 2. Smooth Scroll
+// 6. Smooth Scroll
 // ----------------
 
 var scroll = new SmoothScroll('a[href*="#"]');
 
-
-// 2. Animate on Scroll
+// 7. Animate on Scroll
 // --------------------
 
 $(function() {
@@ -139,5 +132,4 @@ $(function() {
 });
 
 
-  });
-
+});
