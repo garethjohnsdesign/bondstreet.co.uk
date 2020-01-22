ff237b942ae360f251d8a0ab0ef12907db4d1a27
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-01-22 17:23:04 +0000

    Tweak Hero and Scroll to use Viewheight

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -29,6 +29,23 @@ Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min
 $(document).foundation();
 
 
+// 4. Viewport Height Fix
+// ----------------------
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+const vh = window.innerHeight * 0.01;
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
 // 4. Lightgallery
 // ---------------
 
