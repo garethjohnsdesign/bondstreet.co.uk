d4c4a1255d5d560a0d726aaa98255de110fa8116
Gareth Johns <garethjohns@gareths-imac.lan>
2020-11-23 15:55:45 +0000

    Proxy Testing

---
+++ config/instagramfeed.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // User name or hash tag to fetch from Instagram.
+    'instagramUser' => 'stockton_walker',
+
+    // Use codemonauts proxy to get the Instagram page
+    'useProxy' => false,
+
+    // The proxy key to use for authentication
+    'proxyKey' => '',
+
+    // Timeout in seconds waiting for the Instagram page to load.
+    'timeout' => 5,
+
+    // Use Guzzle instead of php's file stream
+    'useGuzzle' => false,
+
+    // Dump Instagram response to file for debugging purpose
+    'dump' => false,
+
+    // Using your own user agent string, remove this array key to use a common user agent of a well known browser
+    'userAgent' => '',
+];
